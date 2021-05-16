13426a424b4829953d80ba56d1ea6e8126f92410
Marlos Duarte Moraes <marlosmoraes@gmail.com>
2021-05-16 18:47:29 -0300

    Modelos, migrações e seeds

--- api/index.js
+++ api/index.js
@@ -1,5 +1,18 @@
 const express = require('express')
+
+
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+const port = 3000
+
+app.get('/teste', (req, res) => res
+    .status(200)
+    .send({ mensagem: 'boas vindas à API'}
+))
+
+app.listen(port, () => console.log(`O servidor esta rodado na porta ${port}`))
+
+module.exports = app
