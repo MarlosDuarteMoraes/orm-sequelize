86bfe155b3a74b9b06507a4fb02b8a77c9905b43
Marlos Duarte Moraes <marlosmoraes@gmail.com>
2021-05-20 14:24:19 -0300

    Soft delete ou exclusão suave

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -60,6 +60,15 @@ class PessoaController {
       return res.status(500).json(error.message)
     }
   }
+  static async restauraPessoa(req, res){
+    const { id } = req.params
+    try {
+      await database.Pessoas.restore({where: { id: Number(id) }})
+      return res.status(200).json({ mensagem: `id ${id} restaurado`})
+    } catch (error) {
+      return res.status(500).json(error.message)
+    }
+  }
   // http://localhost:3000/pessoas/:estudanteId/matricula/:matriculaId
   static async pegaUmaMatricula(req, res) {
     const { estudanteId, matriculaId } = req.params
@@ -118,6 +127,21 @@ class PessoaController {
       return res.status(500).json(error.message)
     }
   }
+  static async restauraMatricula(req, res) {
+    const { estudanteId, matriculaId } = req.params
+    try {
+      await database.Matriculas.restore({
+        where: {
+          id: Number(matriculaId),
+          estudante_id: Number(estudanteId)
+        }
+      })
+      return res.status(200).json({ mensagem: `id ${id} restaurado`})
+    } catch (error) {
+      return res.status(500).json(error.message)
+    }
+  }
+ 
 
 }
 
