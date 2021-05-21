e5032e3f599a58e6dcd63c80c7e2439b74329941
Marlos Duarte Moraes <marlosmoraes@gmail.com>
2021-05-21 13:24:50 -0300

    Incluido escopo de associação e operadores

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -11,11 +11,24 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       Pessoas.hasMany(models.Turmas, { foreignKey: 'docente_id' })
-      Pessoas.hasMany(models.Matriculas, { foreignKey: 'estudante_id' })
+      Pessoas.hasMany(models.Matriculas, { 
+        foreignKey: 'estudante_id',
+        scope: {
+          status: 'confirmado'
+        },
+        as: 'aulasMatriculadas'
+       })
     }
   };
   Pessoas.init({
-    nome: DataTypes.STRING,
+    nome: {
+      type: DataTypes.STRING,
+      validate: {
+        funcaoValidadora: function(dado) {
+          if(dado.length < 3) throw new Error('o campo nome deve ter mais de 3 caracteres')
+        }
+      }
+    },
     ativo: DataTypes.BOOLEAN,
     email: {
       type: DataTypes.STRING,
