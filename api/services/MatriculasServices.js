799ce266e56ed099edb2356d6a11652a3b98f90c
Marlos Duarte Moraes <marlosmoraes@gmail.com>
2021-05-23 17:50:54 -0300

    Refatoração com servicos

---
+++ api/services/MatriculasServices.js
@@ -0,0 +1,9 @@
+const Services = require('./Services')
+
+class MatriculasServices extends Services {
+    constructor(){
+        super('Matriculas')
+    }
+    //métodos específicos do controlador de Matriculas
+}
+module.exports = MatriculasServices
